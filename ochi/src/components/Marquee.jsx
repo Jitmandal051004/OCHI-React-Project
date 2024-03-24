f08ae0451c65c2bd09ea8b0919e7c9ba23ae3323
indrajit-mandal <102886330+Jitmandal051004@users.noreply.github.com>
2024-03-24 06:33:47 +0530

    Eyes component completed

--- ochi/src/components/Marquee.jsx
+++ ochi/src/components/Marquee.jsx
@@ -1,12 +1,13 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import { Parallax } from 'react-scroll-parallax'
 
 function Marquee() {
     return (
         <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43] overflow-hidden'>
             <div className='border-t-2 border-b-2 border-zinc-300 flex justify-between whitespace-nowrap'>
-                <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: 2, duration: 2}} className='text-[20vw] -tracking-[0.085em] pt-4 pb-5 uppercase leading-none font-roboto font-semibold'>we are ochi</motion.h1>
-                <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: 2, duration: 2}} className='text-[20vw] -tracking-[0.085em] pt-4 pb-5 uppercase leading-none font-roboto font-semibold'>we are ochi</motion.h1>
+                <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 8.5}} className='text-[20vw] -tracking-[0.085em] pt-4 pb-5 uppercase leading-none font-roboto font-semibold'>we are ochi</motion.h1>
+                <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 8.5}} className='text-[20vw] -tracking-[0.085em] pt-4 pb-5 uppercase leading-none font-roboto font-semibold'>we are ochi</motion.h1>
             </div>
         </div>
     )
